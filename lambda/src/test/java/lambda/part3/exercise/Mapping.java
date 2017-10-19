923a00b55dc727091dcbce3da80a7a0b766325f8
MrToady <t.v.moisiuk@gmail.com>
2017-10-19 15:51:03 +0300

    Fix "Почему не реализован?"

--- lambda/src/test/java/lambda/part3/exercise/Mapping.java
+++ lambda/src/test/java/lambda/part3/exercise/Mapping.java
@@ -30,14 +30,16 @@ public class Mapping {
 
         // ([T], T -> R) -> [R]
         public <R> MapHelper<R> map(Function<T, R> f) {
-            // TODO
-            throw new UnsupportedOperationException();
+            List<R> result = new ArrayList<>(list.size());
+            list.forEach(t -> result.add(f.apply(t)));
+            return new MapHelper<R>(result);
         }
 
         // ([T], T -> [R]) -> [R]
         public <R> MapHelper<R> flatMap(Function<T, List<R>> f) {
-            // TODO
-            throw new UnsupportedOperationException();
+            List<R> result = new ArrayList<>(list.size());
+            list.forEach(t -> result.addAll(f.apply(t)));
+            return new MapHelper<R>(result);
         }
     }
 
@@ -62,11 +64,10 @@ public class Mapping {
         );
 
         List<Employee> mappedEmployees = new MapHelper<>(employees)
-                /*
-                .map(TODO) // Изменить имя всех сотрудников на John .map(e -> e.withPerson(e.getPerson().withFirstName("John")))
-                .map(TODO) // Добавить всем сотрудникам 1 год опыта .map(e -> e.withJobHistory(addOneYear(e.getJobHistory())))
-                .map(TODO) // Заменить все qa на QA
-                * */
+
+                .map(e -> e.withPerson(e.getPerson().withFirstName("John")))
+                .map(e -> e.withJobHistory(addOneYear(e.getJobHistory())))
+                .map(e -> e.withJobHistory(changeQaToQa(e.getJobHistory())))
                 .getList();
 
         List<Employee> expectedResult = Arrays.asList(
@@ -90,9 +91,21 @@ public class Mapping {
         assertEquals(mappedEmployees, expectedResult);
     }
 
+    private List<JobHistoryEntry> changeQaToQa(List<JobHistoryEntry> jh) {
+        return new MapHelper<>(jh).map(e -> e.getPosition().equals("qa") ? e.withPosition("QA") : e).getList();
+    }
+
+    private List<JobHistoryEntry> addOneYear(List<JobHistoryEntry> jh) {
+        return new MapHelper<>(jh).map(e -> e.withDuration(e.getDuration() + 1)).getList();
+    }
+
     private static class LazyMapHelper<T, R> {
+        private final List<T> list;
+        private final Function<T, R> function;
 
         public LazyMapHelper(List<T> list, Function<T, R> function) {
+            this.list = list;
+            this.function = function;
         }
 
         public static <T> LazyMapHelper<T, T> from(List<T> list) {
@@ -100,17 +113,14 @@ public class Mapping {
         }
 
         public List<R> force() {
-            // TODO
-            throw new UnsupportedOperationException();
+            return new MapHelper<T>(list).map(function).getList();
         }
 
         public <R2> LazyMapHelper<T, R2> map(Function<R, R2> f) {
-            // TODO
-            throw new UnsupportedOperationException();
+            return new LazyMapHelper<T, R2>(list, function.andThen(f));
         }
     }
 
-    // TODO * LazyFlatMapHelper
 
     @Test
     public void lazyMapping() {
@@ -134,13 +144,10 @@ public class Mapping {
                         new JobHistoryEntry(5, "qa", "epam")
                 ))
         );
-
         List<Employee> mappedEmployees = LazyMapHelper.from(employees)
-                /*
-                .map(TODO) // Изменить имя всех сотрудников на John .map(e -> e.withPerson(e.getPerson().withFirstName("John")))
-                .map(TODO) // Добавить всем сотрудникам 1 год опыта .map(e -> e.withJobHistory(addOneYear(e.getJobHistory())))
-                .map(TODO) // Заменить все qu на QA
-                */
+                .map(e -> e.withPerson(e.getPerson().withFirstName("John"))) // Изменить имя всех сотрудников на John .map(e -> e.withPerson(e.getPerson().withFirstName("John")))
+                .map(e -> e.withJobHistory((addOneYear(e.getJobHistory())))) // Добавить всем сотрудникам 1 год опыта .map(e -> e.withJobHistory(addOneYear(e.getJobHistory())))
+                .map(e -> e.withJobHistory(changeQaToQa(e.getJobHistory())))  // Заменить все qu на QA
                 .force();
 
         List<Employee> expectedResult = Arrays.asList(
@@ -160,7 +167,6 @@ public class Mapping {
                         new JobHistoryEntry(6, "QA", "epam")
                 ))
         );
-
         assertEquals(mappedEmployees, expectedResult);
     }
 }
